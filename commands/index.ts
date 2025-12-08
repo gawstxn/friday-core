6ab0e716b8f3240eee9af397e619584f13ea0024
gawstxn <naruenat.privatez@gmail.com>
2025-12-08 21:31:58 +0700

    feat: register new command for handling user registration

--- commands/index.ts
+++ commands/index.ts
@@ -1,7 +1,9 @@
 // commands/index.ts
 import { Command } from '@/types/discord'
 import { pingCommand } from './ping'
+import { RegisterCommand } from './register'
 
 export const commands: Record<string, Command> = {
   [pingCommand.data.name]: pingCommand,
+  [RegisterCommand.data.name]: RegisterCommand,
 }
